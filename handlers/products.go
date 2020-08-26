80dc4cd15e263e17134f53af61be1246a238bbbf
Yashanshu Bhardwaj <yashansh3b@gmail.com>
2020-08-26 18:12:51 +0530

    simple get request

---
+++ handlers/products.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"github.com/yashanshu/product-api/data"
+	"net/http"
+)
+
+// Product is a http.Handler
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a products handler with the giver logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// ServeHTTP is the main entry point for the handler and satisfies the
+// http.Handler interface
+func (p *Products) ServeHTTP(rw http.ResponseWriter. r *http.Request) {
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter. r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// feth the products from the data store
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal JSON", http.StatusInternalServerError)
+	}
+}
